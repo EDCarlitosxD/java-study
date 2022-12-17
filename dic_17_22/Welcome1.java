4fbbd420c382c9488979cf4225832a7c2e9fb098
EDCarlitosxD <juanuchdzib@gmail.com>
2022-12-17 12:34:41 -0600

    Starting study

---
+++ dic_17_22/Welcome1.java
@@ -0,0 +1,14 @@
+package dic_17_22;
+
+public class Welcome1 {
+
+    // method main() this method execute the JAVA application
+    public static void main(String[] args) {
+
+        //Print a message in screen.
+        System.out.println("Hello world");
+        
+        //End method main
+    }
+
+} // End class Welcome1
